8f04424c6336a02be838722e799d11de195893bd
PedroUNB <pedro.oliveira.unb@gmail.com>
2021-02-23 00:13:44 -0300

    statelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,24 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(Container(
-    child: Center(
-      child: Text(
-        'Flutter',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(color: Colors.white, fontSize: 50.0),
+  runApp(AppWidget(title: "Flutter"));
+}
+
+class AppWidget extends StatelessWidget {
+  final String title;
+
+  const AppWidget({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text(
+          title,
+          textDirection: TextDirection.ltr,
+          style: TextStyle(color: Colors.white, fontSize: 50.0),
+        ),
       ),
-    ),
-  ));
+    );
+  }
 }
